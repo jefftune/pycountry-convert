6c94ee41338f765b8e4936664933fa30cb60acbd
Jeff Tanner <jefft@tune.com>
2017-03-12 22:47:35 +0200

    [1145] (pycountry-convert) cleanup

--- pycountry_convert/__init__.py
+++ pycountry_convert/__init__.py
@@ -4,8 +4,8 @@
 #  @namespace pycountry-convert
 
 __title__ = 'pycountry-convert'
-__version__ = '0.2.0'
-__build__ = 0x000200
+__version__ = '0.2.1'
+__build__ = 0x000201
 __version_info__ = tuple(__version__.split('.'))
 
 __author__ = 'jefft@tune.com'
